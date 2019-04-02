d9ac6cc4dda248d51c77f998e227a6f83d9b0455
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-04-01 19:14:36 -0700

    start: Building up the very basic DI

--- app/src/main/java/com/idiotleon/demodiwdagger2/MainActivity.java
+++ app/src/main/java/com/idiotleon/demodiwdagger2/MainActivity.java
@@ -1,22 +1,19 @@
 package com.idiotleon.demodiwdagger2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.idiotleon.demodiwdagger2.car.Car;
 import com.idiotleon.demodiwdagger2.car.CarComponent;
 import com.idiotleon.demodiwdagger2.car.DaggerCarComponent;
-import com.idiotleon.demodiwdagger2.car.Engine;
-import com.idiotleon.demodiwdagger2.car.Wheels;
 
 public class MainActivity extends AppCompatActivity {
 
     private Car car;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         CarComponent component = DaggerCarComponent.create();
         car = component.getCar();
